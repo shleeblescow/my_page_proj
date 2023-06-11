f3455da2ec697b77bb02f5ebe66882880459ccc4
Shelby Lee <shleeblesgreencow@gmail.com>
2023-06-11 10:43:08 -0700

    delete button working

--- client/src/components/renderPostCard.js
+++ client/src/components/renderPostCard.js
@@ -1,4 +1,24 @@
+import { useNavigate, useParams, Link } from 'react-router-dom';
+
 export default function RenderPostCard({ thisPost, isAdmin }) {
+
+    function deletePostDrama() {
+
+        fetch(`/posts/${thisPost.id}`,{
+            method:'DELETE'
+        })
+        .then(res => {
+            if (res.ok) {
+                console.log('yeah glad you deleted that that was such a stupid post')
+            } else {
+                //res.json().then(json => setErrors(Object.entries(json.errors)))
+                res.json().then(retErrors => {
+                    // setErrors(Object.entries(retErrors.errors))
+                    console.log('nice try dumb dumb')
+                })
+            }
+        })
+    }
 
 
     return (
@@ -8,8 +28,8 @@ export default function RenderPostCard({ thisPost, isAdmin }) {
             <h3>{thisPost.body}</h3>
 
             {isAdmin ?
-                <button>
-                    yeah we're cool
+                <button onClick={deletePostDrama}>
+                    what a stupid post
                 </button>
                 :
                 <h1>nothing for you stupid</h1>
